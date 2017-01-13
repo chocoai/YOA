939a44c74d0ec5abc7a7123676e6a5f63d22421c
yangbinqi <ybq@outlook.com>
2017-01-13 18:55:03 +0800

    fix UI

--- src/Web/WorkFlow/NowWorkFlow.aspx
+++ src/Web/WorkFlow/NowWorkFlow.aspx
@@ -182,6 +182,7 @@
                                 <ItemStyle HorizontalAlign="Center" />
                             </asp:TemplateField>
                         </Columns>
+                        <PagerSettings Visible="False" />
                         <RowStyle HorizontalAlign="Center" Height="25px" />
                         <EmptyDataTemplate>
                             <table border="0" cellpadding="0" cellspacing="0" width="100%">
--- src/Web/WorkFlow/PublicSeal.aspx.cs
+++ src/Web/WorkFlow/PublicSeal.aspx.cs
@@ -8,6 +8,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.HtmlControls;
+using Wuqi.Webdiyer;
 
 public partial class WorkFlow_PublicSeal : System.Web.UI.Page
 {
@@ -36,119 +37,40 @@ public partial class WorkFlow_PublicSeal : System.Web.UI.Page
             }
         }
     }
-    public void DataBindToGridview()
-    {
+    protected void AspNetPager1_PageChanged(object src, EventArgs e) {
+        DataBindToGridview();
+    }
+    public void DataBindToGridview() {
+        GVData.PageSize = AspNetPager1.PageSize;
+        GVData.PageIndex = AspNetPager1.CurrentPageIndex - 1;
         ZWL.BLL.ERPYinZhang MyModel = new ZWL.BLL.ERPYinZhang();
+        DataSet ds = new DataSet();
         if (Request.QueryString["Type"].ToString() == "公共印章")
         {
-            GVData.DataSource = MyModel.GetList("YinZhangName Like '%" + this.TextBox1.Text + "%' and YinZhangLeiBie='公共印章' order by ID desc");
+             ds = MyModel.GetList("YinZhangName Like '%" + this.TextBox1.Text + "%' and YinZhangLeiBie='公共印章' order by ID desc");
         }
         else
         {
-            GVData.DataSource = MyModel.GetList("YinZhangName Like '%" + this.TextBox1.Text + "%' and YinZhangLeiBie='私人印章' and UserName='"+ZWL.Common.PublicMethod.GetSessionValue("UserName")+"' order by ID desc");
+             ds = MyModel.GetList("YinZhangName Like '%" + this.TextBox1.Text + "%' and YinZhangLeiBie='私人印章' and UserName='"+ZWL.Common.PublicMethod.GetSessionValue("UserName")+"' order by ID desc");
         }
+        GVData.DataSource = ds;
         GVData.DataBind();
-        LabPageSum.Text = Convert.ToString(GVData.PageCount);
-        LabCurrentPage.Text = Convert.ToString(((int)GVData.PageIndex + 1));
-        this.GoPage.Text = LabCurrentPage.Text.ToString();
+        AspNetPager1.RecordCount = ds.Tables[0].Rows.Count;
     }
-    #region  分页方法
-    protected void ButtonGo_Click(object sender, ImageClickEventArgs e)
-    {
-        try
-        {
-            if (GoPage.Text.Trim().ToString() == "")
-            {
-                Response.Write("<script language='javascript'>alert('页码不可以为空!');</script>");
-            }
-            else if (GoPage.Text.Trim().ToString() == "0" || Convert.ToInt32(GoPage.Text.Trim().ToString()) > GVData.PageCount)
-            {
-                Response.Write("<script language='javascript'>alert('页码不是一个有效值!');</script>");
-            }
-            else if (GoPage.Text.Trim() != "")
-            {
-                int PageI = Int32.Parse(GoPage.Text.Trim()) - 1;
-                if (PageI >= 0 && PageI < (GVData.PageCount))
-                {
-                    GVData.PageIndex = PageI;
-                }
-            }
-
-            if (TxtPageSize.Text.Trim().ToString() == "")
-            {
-                Response.Write("<script language='javascript'>alert('每页显示行数不可以为空!');</script>");
-            }
-            else if (TxtPageSize.Text.Trim().ToString() == "0")
-            {
-                Response.Write("<script language='javascript'>alert('每页显示行数不是一个有效值!');</script>");
-            }
-            else if (TxtPageSize.Text.Trim() != "")
-            {
-                try
-                {
-                    int MyPageSize = int.Parse(TxtPageSize.Text.ToString().Trim());
-                    this.GVData.PageSize = MyPageSize;
-                }
-                catch
-                {
-                    Response.Write("<script language='javascript'>alert('每页显示行数不是一个有效值!');</script>");
-                }
-            }
-
-            DataBindToGridview();
-        }
-        catch
-        {
-            DataBindToGridview();
-            Response.Write("<script language='javascript'>alert('请输入有效数字！');</script>");
-        }
-    }
-    protected void PagerButtonClick(object sender, ImageClickEventArgs e)
-    {
-        //获得Button的参数值
-        string arg = ((ImageButton)sender).CommandName.ToString();
-        switch (arg)
-        {
-            case ("Next"):
-                if (this.GVData.PageIndex < (GVData.PageCount - 1))
-                    GVData.PageIndex++;
-                break;
-            case ("Pre"):
-                if (GVData.PageIndex > 0)
-                    GVData.PageIndex--;
-                break;
-            case ("Last"):
-                try
-                {
-                    GVData.PageIndex = (GVData.PageCount - 1);
-                }
-                catch
-                {
-                    GVData.PageIndex = 0;
-                }
-
-                break;
-            default:
-                //本页值
-                GVData.PageIndex = 0;
-                break;
-        }
-        DataBindToGridview();
-    }
-    #endregion
+    
     protected void GVData_RowDataBound(object sender, GridViewRowEventArgs e)
     {
         ZWL.Common.PublicMethod.GridViewRowDataBound(e);
     }
-    protected void ImageButton4_Click(object sender, ImageClickEventArgs e)
+    protected void ImageButton4_Click(object sender, EventArgs e)
     {
         DataBindToGridview();
     }
-    protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
+    protected void ImageButton1_Click(object sender, EventArgs e)
     {
         Response.Redirect("YinZhangAdd.aspx?Type="+Request.QueryString["Type"].ToString());
     }
-    protected void ImageButton3_Click(object sender, ImageClickEventArgs e)
+    protected void ImageButton3_Click(object sender, EventArgs e)
     {
         string IDlist = ZWL.Common.PublicMethod.CheckCbx(this.GVData, "CheckSelect", "LabVisible");
         if (ZWL.DBUtility.DbHelperSQL.ExecuteSQL("delete from ERPYinZhang where ID in (" + IDlist + ")") == -1)
@@ -166,7 +88,7 @@ public partial class WorkFlow_PublicSeal : System.Web.UI.Page
             MyRiZhi.Add();
         }
     }
-    protected void ImageButton2_Click(object sender, ImageClickEventArgs e)
+    protected void ImageButton2_Click(object sender, EventArgs e)
     {
         string IDList = "0";
         for (int i = 0; i < GVData.Rows.Count; i++)
@@ -181,7 +103,7 @@ public partial class WorkFlow_PublicSeal : System.Web.UI.Page
         MyTable.Add("TimeStr", "创建时间");
         ZWL.Common.DataToExcel.GridViewToExcel(ZWL.DBUtility.DbHelperSQL.GetDataSet("select YinZhangName,YinZhangLeiBie,UserName,TimeStr from ERPYinZhang where ID in (" + IDList + ") order by ID desc"), MyTable, "Excel报表");
     }
-    protected void ImageButton5_Click(object sender, ImageClickEventArgs e)
+    protected void ImageButton5_Click(object sender, EventArgs e)
     {
         string CheckStr = ZWL.Common.PublicMethod.CheckCbx(this.GVData, "CheckSelect", "LabVisible");
         string[] CheckStrArray = CheckStr.Split(',');
